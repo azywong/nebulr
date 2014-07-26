0c89a411185cce4d24b72c6acfd0ac8ded27c799
allison <azywong@gmail.com>
2014-07-26 12:34:33 -0700

    implemented user login and sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :name
+  attr_accessible :username, :name, :password
   has_many :matches, :foreign_key => "asker_id",
       :class_name => "Match"
   has_many :answerers, :through => :matches
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Nebulr::Application.routes.draw do
   root to:'home#index'
 
   resources :users
+  match 'sessions/new' => 'sessions#new'
+  match 'sessions/create' => 'sessions#create'
+  match 'sessions/exit' => 'sessions#exit'
 
   resources :questions do
     resources :answers
