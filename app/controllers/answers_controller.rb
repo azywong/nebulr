615852a6299bc3b8e0ef9c42487fa1642ea434ac
allison <azywong@gmail.com>
2014-07-26 13:43:06 -0700

    answers implemented

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,15 @@
+class AnswersController < ApplicationController
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    user = User.find(session[:id])
+    @question = Question.find(params["question_id"].to_i)
+    @answer = Answer.create(params["answer"])
+    @question.answers << @answer
+    user.answers << @answer
+    redirect_to question_url(@question)
+  end
+end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,11 +9,13 @@ class QuestionsController < ApplicationController
 
   def create
     user = User.find(session[:id])
-    question = Question.create(params["question"])
-    user.questions << question
-    redirect_to
+    @question = Question.create(params["question"])
+    user.questions << @question
+    redirect_to question_url(@question)
   end
 
   def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
   end
 end
