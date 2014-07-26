017eb39029e2e9166ce20951af8b8c2fc8323fe7
allison <azywong@gmail.com>
2014-07-26 11:34:32 -0700

    initial commit with correct rails version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nebulr::Application.config.secret_token = 'a95bf614957160b57de4a4547f13875eb647644f812559fe7fcb279833f376c17fc002b3b50d0dc196cdeca5217e0bc8e77b3d5e78447c8cb63015546f8546c9'
