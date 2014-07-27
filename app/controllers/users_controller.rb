4b29b182a95bec7a30bdf8cca1f82248fae96a4a
allison <azywong@gmail.com>
2014-07-26 18:41:26 -0700

    styling and clarity changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(params)
-    if user.save
+    @user = User.new(params[:user])
+    if @user.save
+      session[:id] = @user.id
       redirect_to root_path
     else
       render :new
